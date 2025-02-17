b0741f02501487a02f7cc92af8177bd3c1f23de8
Waris Hezb <waris.hezb@Digitals-MacBook-Air-5.local>
2025-02-17 13:55:47 +0000

    Initial GameLogic

--- src/main/scala/GameLogic.scala
+++ src/main/scala/GameLogic.scala
@@ -1,11 +1,29 @@
 class GameLogic {
 
-//  var remainingCharacters: List[Character] = GameData.characters
-//  val secretCharacter: Character = remainingCharacters(scala.util.Random.nextInt(remainingCharacters.length))
-//
-//  def askQuestion(question:String):
+  var remainingCharacters: List[Character] = GameData.characters
+  val secretCharacter: Character = remainingCharacters(scala.util.Random.nextInt(remainingCharacters.length))
 
+  def askQuestion(question:String): List[Character] = {
 
+    val newRemainingCharacters = QuestionFilter.filterCharacters(remainingCharacters,question, secretCharacter)
+    remainingCharacters = newRemainingCharacters
+    remainingCharacters
+
+  }
+  def guessCharacter(name:String):Boolean = {
+    val guessedCorrectly = name == secretCharacter.name
+    if (guessedCorrectly) {
+      remainingCharacters = List(secretCharacter)    //Only correct answer remains
+      println("Congratulations you guess correctly ")
+
+    } else {
+      remainingCharacters = remainingCharacters.filterNot(_.name == name)  ///remove incorrect guess
+      println("Incorrect guess, ask a question again")
+
+    }
+    guessedCorrectly
+
+  }
 }
 
 
