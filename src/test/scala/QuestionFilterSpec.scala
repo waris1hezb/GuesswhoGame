e5d478d751f7d4e8842b064aa6bd4b92a9fa48cb
Dorothea Toeller <dorothea.toeller@mac.home>
2025-02-18 12:01:26 +0000

    tests for Boolean values in filterCharacters method

--- src/test/scala/QuestionFilterSpec.scala
+++ src/test/scala/QuestionFilterSpec.scala
@@ -9,15 +9,29 @@ class QuestionFilterSpec extends AnyWordSpec with Matchers {
   val secretCharacter: Character = Character("Ronaldo", "Male", "Black", hasGlasses = true, hasHat = false)
 
   "filterCharacters" should {
-    "return a Boolean and a list of characters wearing glasses" when {
+    //test if the secret character has glasses and return a list of all characters that match his hasGlasses boolean
+    "return a boolean and a list of characters wearing glasses" when {
       "the characters wear glasses" in {
-        val question = "Does the character wear glasses?"
+        val question = "does the person have glasses?"
 
         //call the method
         val result = filterCharacters(characters, question, secretCharacter)
 
         //expected result
-        val expectedResult = (true, List(characters.head, characters(3), characters(5)))
+        val expectedResult = (true, List(characters.head, characters(2), characters(4)))
+
+        result shouldBe expectedResult
+      }
+    }
+
+    //test if the secret character has glasses and return a list of all characters that match his hasHat boolean
+    "return a boolean and a list of characters wearing a hat" when {
+      "the characters wear a hat" in {
+        val question = "do they wear a hat?"
+
+        val result = filterCharacters(characters, question, secretCharacter)
+
+        val expectedResult = (false, List(characters(1), characters(2), characters(4)))
 
         result shouldBe expectedResult
       }
