147cb3c2852d0d57a7d793093b1e1cef5c8cf858
Dorothea Toeller <dorothea.toeller@mac.home>
2025-02-18 08:59:05 +0000

    starts test writing

--- src/test/scala/QuestionFilterSpec.scala
+++ src/test/scala/QuestionFilterSpec.scala
@@ -5,15 +5,18 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class QuestionFilterSpec extends AnyWordSpec with Matchers {
 
-
-
   "filterCharacters" should {
     "return a list of characters wearing glasses" when {
       "they wear glasses." in {
+        val question = "Does the character wear glasses?"
 
+        //call the method
+        val result = filterCharacters(characters, question, secretCharacter)
+        //expected result
+        val expectedResult = List(characters.head, characters(2))
+
+        result shouldBe expectedResult
       }
     }
   }
-
-
 }
