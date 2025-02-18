b94fd9d929e84c0963ba9b4ab8ff62c7d8317490
Dorothea Toeller <dorothea.toeller@mac.home>
2025-02-18 11:07:44 +0000

    tries to fix testing issue

--- src/test/scala/QuestionFilterSpec.scala
+++ src/test/scala/QuestionFilterSpec.scala
@@ -1,3 +1,4 @@
+import GameData.characters
 import QuestionFilter._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -5,15 +6,17 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class QuestionFilterSpec extends AnyWordSpec with Matchers {
 
+  val secretCharacter: Character = Character("Ronaldo", "Male", "Black", hasGlasses = true, hasHat = false)
+
   "filterCharacters" should {
-    "return a list of characters wearing glasses" when {
-      "they wear glasses." in {
+    "return a Boolean and a list of characters wearing glasses" when {
+      "the characters wear glasses" in {
         val question = "Does the character wear glasses?"
 
         //call the method
         val result = filterCharacters(characters, question, secretCharacter)
         //expected result
-        val expectedResult = List(characters.head, characters(2))
+        val expectedResult = true; characters(0); characters(3); characters(5)
 
         result shouldBe expectedResult
       }
